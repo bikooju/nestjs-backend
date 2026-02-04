54f0bcf54b2b5c6df8394edb0bf1c17e64768c02
bikooju <jhjk1234@gmail.com>
2026-02-04 23:36:28 +0900

    :gear: 데이터베이스 설정

---
+++ src/database/database.config.ts
@@ -0,0 +1,25 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+    return {
+      type: 'mysql',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_DATABASE'),
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: configService.get<string>('NODE_ENV') === 'development',
+      logging: configService.get<string>('NODE_ENV') === 'development',
+      timezone: 'Z',
+      charset: 'utf8mb4',
+    };
+  },
+};
